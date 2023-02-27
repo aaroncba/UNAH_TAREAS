c43645ec99c44bfc96292d7471aec1c27e0261b0
Aaron Isaac Colindres Barralaga <aaron.co.ba03@gmail.com>
2023-02-27 13:02:40 -0600

    Functions added

--- src/tarea2.java
+++ src/tarea2.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class tarea2 {
+public class tarea2 {
+    public static void main(String[] args) {
+
+    }
+
+    //3n+1 function || Collatz Function
+    /*
+    * Esta funcion va a tomar un valor y seguira lo siguente hasta llegar a 1
+    * -> n%2 == 0 ->> n/2
+    * -> n%2 != 0 ->> 3n+1
+    * hacer ciclo hasta llegar a 0
+    * */
+    public void Collatz(){}
+
+
+    //parrafo function
+    /*
+    * Esta funcion va a tomar un string y un int como paramentro:
+    * Cada linea va a tener un o mas palabras, que su total de letras (incluyendo el espacio) sean el numero que se dio como paramentro pero con la palabra completa
+    * Una vez se complete un linea, se va a pasar a la proxima y se hara lo mismo
+    * */
+    public void Parrafo(String frase, int margen){}
+
+
+    //triangulo function
+    /*
+    * Esta funcion va a tomar un char y un int (n) como paramentro
+    * Lo que hara con esto es un triangulo invertido, la primer fila tendra el tamaño del int que se dio como paramentro mas un espacio entre cada char
+    * La siguente fila sera n - el numero de fila, esto nos dira la cantidad de chars que se tienen que agregar, la cantidad de espacios al inicio de la fila se calculan con
+    * -> n - (n - numero de fila)
+    * Se estara repitiendo esto hasta llegar a 0
+    * */
+    public void Triangulo(char figura, int base){}
 }
